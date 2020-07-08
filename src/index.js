6457574d0766d804f8090851d2ec69d48a49926f
betonit007 <timnagorski@hotmail.com>
2020-07-07 20:45:04 -0400

    install npm serve --s and changed startup scripts

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ import App from './App'
 ReactDOM.render(
     <App />, document.getElementById("root")
 )
+
+//   npm install serve --s
+
+// "scripts": {
+    // "dev": "react-scripts start",
+    // "start": "serve -s build",
+    // "build": "react-scripts build",
+    // "test": "react-scripts test --env=jsdom",
+    // "eject": "react-scripts eject",
+    // "heroku-postbuild": "npm run build"
+//     }
